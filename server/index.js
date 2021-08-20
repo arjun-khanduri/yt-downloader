16dcace9798217e2cce72c8089169f7bcb95ad0f
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-08-20 20:26:42 +0530

    Installed nodemon

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const ytdl = require('ytdl-core');
 
 const PORT = process.env.PORT || 8000;
 
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.json())
+
+app.get('/download', (req, res) => {
+    ytdl('http://www.youtube.com/watch?v=aqz-KE-bpKQ')
+        .pipe(fs.createWriteStream('video.mp4'))
+})
+
 app.listen(PORT, (req, res) => {
     console.log(`Listening on port ${PORT}`);
-})
+});
