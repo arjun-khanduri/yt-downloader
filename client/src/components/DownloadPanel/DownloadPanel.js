da0cfde757f736a917f0f6e3dd3deb01d472e2ce
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-08-21 19:44:29 +0530

    Successfully displaying video info on the client side

--- client/src/components/DownloadPanel/DownloadPanel.js
+++ client/src/components/DownloadPanel/DownloadPanel.js
@@ -3,6 +3,8 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import axios from 'axios';
+import Divider from '@material-ui/core/Divider';
+import ViewResultPanel from '../ViewResultPanel/ViewResultPanel';
 
 const useStyles = makeStyles((theme) => ({
     button: {
@@ -10,25 +12,50 @@ const useStyles = makeStyles((theme) => ({
     },
     textField: {
         width: '50%',
+    },
+    divider: {
+        marginTop: '30px',
+        marginLeft: '20%',
+        marginRight: '20%',
     }
 }));
 
 
 const DownloadPanel = () => {
     const [link, setLink] = useState('')
+    const [progress, setProgress] = useState(0)
+    const [videoInfo, setVideoInfo] = useState([])
     const fetchVideo = () => {
-        axios.post("http://localhost:8000/download", {link: link})
+        axios.get(`http://localhost:8000/fetch?link=${link}`)
+        .then(response => {
+            setVideoInfo(response.data)
+        });
     }
     const classes = useStyles();
     return (
         <>
             <div>
-                <TextField id="ytLink" className={classes.textField} label="YouTube Video Link" variant="outlined" onChange={(e) => { setLink(e.target.value) }} />
+                <TextField
+                    id="ytLink"
+                    className={classes.textField}
+                    label="YouTube Video Link"
+                    variant="outlined"
+                    onChange={(e) => { setLink(e.target.value) }} />
             </div>
             <div>
-                <Button variant="contained" onClick={fetchVideo} className={classes.button} color="primary">
+                <Button
+                    variant="contained"
+                    onClick={fetchVideo}
+                    className={classes.button}
+                    color="primary">
                     Fetch video
                 </Button>
+            </div>
+            <div>
+                <Divider variant="middle" className={classes.divider} />
+                <ViewResultPanel />
+                <h1>{videoInfo.title}</h1>
+                <img src={videoInfo.thumbnail} alt="Video Thumbnail"/>
             </div>
         </>
     )
--- server/index.js
+++ server/index.js
@@ -7,14 +7,39 @@ const ytdl = require('ytdl-core');
 
 const PORT = process.env.PORT || 8000;
 
-app.use(bodyParser.urlencoded({extended: true}));
+let progress = 0;
+let video;
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-app.post('/download', (req, res) => {
-    const link = req.body.link;
-    ytdl(`${link}`)
-        .pipe(fs.createWriteStream('video.mp4'))
+app.get('/fetch', async (req, res) => {
+    let link = req.query.link;
+    video = ytdl(link);
+    let videoTitle = '';
+    let videoThumbnail = '';
+    await ytdl.getInfo(ytdl.getURLVideoID(link)).then(info => {
+        videoTitle = info.videoDetails.title;
+        videoThumbnail = info.videoDetails.thumbnails[info.videoDetails.thumbnails.length - 1].url;
+    });
+    res.send({title: videoTitle, thumbnail: videoThumbnail});
+})
+
+
+app.post('/download', async (req, res) => {
+    const video = ytdl(link);
+    ytdl.getInfo(ytdl.getURLVideoID(link)).then(info => {
+        videoTitle = info.videoDetails.title
+        // console.log('rating:', info.player_response.videoDetails.averageRating);
+        // console.log('uploaded by:', info.videoDetails.author.name);
+    });
+    video.pipe(fs.createWriteStream('videoTitle.mp4'));
+    video.on('progress', async (chunkLength, downloaded, total) => {
+        const percent = downloaded / total;
+        progress = (percent * 100).toFixed(2)
+        console.log(progress);
+    });
 })
 
 app.listen(PORT, (req, res) => {
