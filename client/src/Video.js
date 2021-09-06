42a9b1ee78c192ccb1e74972c92361d5f04657b4
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-09-06 18:18:16 +0530

    Routed a component for video downloads

---
+++ client/src/Video.js
@@ -0,0 +1,13 @@
+import './App.css';
+import FetchVideoPanel from './components/FetchVideoPanel/FetchVideoPanel';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>YouTube Video Downloader</h1>
+      <FetchVideoPanel />
+    </div>
+  );
+}
+
+export default App;
