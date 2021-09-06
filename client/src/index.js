c1001a007e80f24570ba3e676c111c5aaaff3170
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-09-06 18:15:22 +0530

    Added routes for video downloads

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Video from './Video';
+import { Switch, Route, Redirect, BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/video" component={Video} />
+      <Route path="/" exact component={App} />
+      <Redirect to="/" />
+    </Switch>
+  </BrowserRouter>
+  ,
   document.getElementById('root')
 );
